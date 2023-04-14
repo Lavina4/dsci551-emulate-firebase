f4d6d1f31578fd728b8471e98f5984f1650a9587
Lavina Agarwal <agarwallavina04@gmail.com>
2023-04-14 15:03:38 -0700

    removed unused imports

--- firebase.py
+++ firebase.py
@@ -1,23 +1,24 @@
-# More details at:
-#     https://flask.palletsprojects.com/en/2.2.x/
-
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 from pymongo import MongoClient
-# import json
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
 def create_projection(path):
-    # path_dict = {path: 1 for path in paths}
-    # path_dict['_id'] = 0
-    # return path_dict
     return {path: 1, '_id': 0}
 
 def get_response(res_dict, paths):
     for path in paths[1:]:
         res_dict = res_dict[path]
     return res_dict
+
+def filtering():
+    # startAt
+    # endAt
+    # equalTo
+    # limitToFirst
+    # limitToLast
+    pass
 
 @app.route('/', defaults={'myPath': ''})
 @app.route('/<path:myPath>', methods=['GET'])
@@ -28,13 +29,13 @@ def get(myPath):
     paths[-1] = paths[-1].removesuffix('.json')
     if paths[-1] == '': ## /.json type of url
         paths.pop()
-    if paths[0]:
+    if paths:
         if len(paths) > 1: ## if paths is longer than 1 then projection is required to select specific data
             path_dict = create_projection(paths[1])
             resp = db.jobs.find({'_id': int(paths[0])}, path_dict)
         else:
             resp = db.jobs.find({'_id': int(paths[0])}, {'_id': 0})
-    else:
+    else: ## empty indicates all the records have to be returned
         resp = db.jobs.find({}, {'_id': 0})
     resp = list(resp)
     if len(resp) == 1 and {} in resp:
